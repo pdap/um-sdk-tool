3b6a43b20026e9a435127df07b29cad614062261
GitHub Action <action@github.com>
2025-02-10 06:56:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/pdap/umsdktool/MyBundle.kt
+++ src/main/kotlin/com/github/pdap/umsdktool/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.pdap.umsdktool
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/pdap/umsdktool/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/pdap/umsdktool/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.pdap.umsdktool.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
